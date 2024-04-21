e9cec8a5196f7cea0e57c49ba367c9ef4df8527c
js336rice <156249455+js336rice@users.noreply.github.com>
2024-04-21 13:32:40 -0500

    Found a better way to do it, run_sim turned back to normal now

--- HouseElevation/src/run_sim.jl
+++ HouseElevation/src/run_sim.jl
@@ -36,13 +36,9 @@ function run_sim(a::Action, sow::SOW, p::ModelParams)
         upfront_cost = construction_cost
         annual_cost = 0
     elseif fin.loan==1   #if taking out a loan
-        if construction_cost > fin.amnt_paid_off #if construction cost is more than we can take out for a loan 
-            return 32  #we just can't explore this scenario
-            #return a positive number since our sims could never possibly return one.
-        else #if we can take out a loan
-            annual_cost = annual_loan_cost(construction_cost, fin.loan_rate, fin.loan_years)
-            upfront_cost = 0  #Have to pay for the first year of the loan/down payment?
-        end
+        annual_cost = annual_loan_cost(construction_cost, fin.loan_rate, fin.loan_years)
+        upfront_cost = 0  #Have to pay for the first year of the loan/down payment?
+
     end
   
         
